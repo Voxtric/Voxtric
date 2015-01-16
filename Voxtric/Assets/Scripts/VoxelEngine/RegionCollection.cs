f1ace8f196bdc542a3446684db57828bb004b30c
Architect-1 <ben@ben-drury.co.uk>
2015-01-16 22:51:16 +0000

    Began work on RegionCollection class

--- Voxtric/Assets/Scripts/VoxelEngine/RegionCollection.cs
+++ Voxtric/Assets/Scripts/VoxelEngine/RegionCollection.cs
@@ -4,6 +4,14 @@ namespace VoxelEngine
 {
     public sealed class RegionCollection : MonoBehaviour
     {
+        Region[,,] _regions;
+        private IntVec3 _dimensions;
 
+        public void Initialise(IntVec3 dimensions, string name)
+        {
+            gameObject.name = name;
+            _dimensions = dimensions;
+            _regions = new Region[dimensions.x, dimensions.y, dimensions.z];
+        }
     }
 }
